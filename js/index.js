f045308b958095612ebd7e9da022ba1fe27bf94b
Gangarig <112808191+Gangarig@users.noreply.github.com>
2023-03-20 05:57:40 +0100

    style adjusted experience on progress

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// method brings navbar down by togglin class
+
 let hamburger = document.getElementById('hamburger');
 let responsive = document.getElementById('responsive');
 
@@ -8,4 +10,22 @@ function navbar() {
     } else {
         hamburger.classList.replace('bi-x-lg', 'bi-list');
     }
+}
+// dark Mode toggler method
+let sun1 = document.getElementById('sun1');
+let sun2 = document.getElementById('sun2');
+document.querySelectorAll('#sun1','#sun2').forEach(function(element) {
+    element.addEventListener('click',darkMode);
+});
+
+function darkMode(){
+    // icon changes on click
+    if (sun1.classList.contains('bi-brightness-high-fill')){
+        sun1.classList.replace('bi-brightness-high-fill','bi-moon-fill');
+        sun2.classList.replace('bi-brightness-high-fill','bi-moon-fill');
+    } else {
+        sun1.classList.replace('bi-moon-fill','bi-brightness-high-fill');
+        sun2.classList.replace('bi-moon-fill','bi-brightness-high-fill');
+    }
+    document.body.classList.toggle('darkmode')
 }
